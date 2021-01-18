8793ecb4b146787b96000a5dcab29683fb84a0c8
vagrant <vagrant@localhost.localdomain>
2021-01-18 03:03:10 +0000

    Car Model作成

--- CalcApp/ViewController.swift
+++ CalcApp/ViewController.swift
@@ -14,11 +14,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        carModel.drive()
+        carModel.frontWheel = 10
+        carModel.rearWheel = 10
         
     }
 
     @IBAction func doAction(_ sender: Any) {
+        
+        carModel.drive()
+        carModel.move(toBack: "後ろに行くよ!!")
+        
+        let total  =  carModel.plusAndMinus(num1: carModel.frontWheel, num2: carModel.rearWheel)
+        print("タイヤの合計数 \(total)")
     }
     
 }
